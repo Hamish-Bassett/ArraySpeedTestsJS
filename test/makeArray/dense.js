66627f7b719c126da1f184e1098ae81b5bf3f331
Hamish Bassett <hamish.bassett@smartvoice.com.au>
2018-12-28 22:36:59 +1100

    building dense array method.

---
+++ test/makeArray/dense.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+
+const sparse = require('../../library/makeArray/dense');
+
+const expect = chai.expect;
+
+describe('dense', () => {
+  const key = 'this is the key';
+  const length = Math.round(Math.random() * 1000);
+  const res = sparse({
+    length,
+    key,
+  });
+  it('should be a function', () => {
+    expect(sparse).to.be.a('function');
+  });
+  it('should return an array', () => {
+    expect(sparse({
+      length: 1,
+    })).to.be.an('array');
+  });
+  it('should be able to produce an array of arbitrary length', () => {
+    expect(res).to.have.lengthOf(length);
+  });
+  it('randomly selected elements should not be null.', () => {
+    for (let count = 0; count < 100; count++) {
+      expect(res[Math.round(Math.random(length))]).to.not.be.undefined;      
+    }
+  });
+  it('should have the key in it', () => {
+    expect(res).to.include(key);
+  });
+});
